59a320645a95d8107762bbeb3e13c6330f2f3648
PARKYOUNGHYUN <bdt02295@naver.com>
2022-03-21 04:16:44 +0900

    로컬 스토리지를 사용한 로그인 처리 추가

---
+++ script/login.js
@@ -0,0 +1,46 @@
+const USER_NAME_KEY = "userName";
+const HIDDEN_CLASS_NAME = "hidden";
+
+const loginForm = document.querySelector("header form");
+const logoutBtn = document.getElementById("logout");
+
+function printUserName() {
+  const loginInfo = document.querySelector("header div");
+  const main = document.querySelector("main");
+
+  const userName = localStorage.getItem(USER_NAME_KEY);
+
+  if (userName) {
+    const h1 = loginInfo.querySelector("h1");
+    h1.innerText = `${userName} Hello!😊`;
+
+    loginForm.classList.add(HIDDEN_CLASS_NAME);
+    loginInfo.classList.remove(HIDDEN_CLASS_NAME);
+  } else {
+    loginForm.classList.remove(HIDDEN_CLASS_NAME);
+    loginInfo.classList.add(HIDDEN_CLASS_NAME);
+  }
+}
+
+function registUserName(e) {
+  e.preventDefault();
+
+  const value = loginForm.querySelector("input").value;
+  localStorage.setItem(USER_NAME_KEY, value);
+
+  printUserName();
+}
+
+function logout() {
+  const input = loginForm.querySelector("input");
+  input.value = "";
+
+  localStorage.removeItem(USER_NAME_KEY);
+
+  printUserName();
+}
+
+document.addEventListener("submit", registUserName);
+logoutBtn.addEventListener("click", logout);
+
+printUserName();
